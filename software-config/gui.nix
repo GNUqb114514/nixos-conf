9bdf03fc3ac62a690f5e42db4eb01cc6b57d01dc
qb114514 <GNUqb114514@outlook.com>
2025-01-27 18:25:49 +0800

    feat(gui): Enable i3blocks
        Currently plain
        Example config

--- software-config/gui.nix
+++ software-config/gui.nix
@@ -1,3 +1,5 @@
 { pkgs, ... }: {
   home.file.".config/niri/config.kdl".source = ../plain/niri.kdl;
+  home.file.".config/i3blocks/config".source = ../plain/i3blocks.ini;
+  home.file.".config/i3bar-river/config.toml".source = ../plain/i3bar-river.toml;
 }
