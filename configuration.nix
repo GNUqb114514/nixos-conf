3eb6a8d24426a7c906b59b6a91cbb5133b891dad
qb114514 <GNUqb114514@outlook.com>
2025-01-28 08:26:18 +0800

    feat(fs): Add rclone

--- configuration.nix
+++ configuration.nix
@@ -82,7 +82,7 @@
     git
     neovim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     wget
-  # rclone
+    rclone
   ];
   environment.variables.EDITOR = "nvim";
 
