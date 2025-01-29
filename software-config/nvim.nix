65cdda5f0c5017f70517e080bbae9b81b5795d2d
qb114514 <GNUqb114514@outlook.com>
2025-01-29 18:24:08 +0800

    feat(nvim): Sync confs from old conf

--- software-config/nvim.nix
+++ software-config/nvim.nix
@@ -1,13 +1,58 @@
 { config, pkgs, ... }: let
-  helpers = config.lib.nixvim;
+helpers = config.lib.nixvim;
 in {
   programs.nixvim = {
     enable = true;
     defaultEditor = true;
     opts = {
+      # Numbers
       relativenumber = true;
       number = true;
+      signcolumn = "yes";
+
+      # Indenting
+      expandtab = true;
+      softtabstop = 4;
+      shiftwidth = 4;
+
+      # Searching
+      hlsearch = true;
+      smartcase = true;
+
+      # Completion
+      wildmenu = true;
+      completeopt = [ "menuone" "popup" "noinsert" "noselect" ];
+
+      # Appearance
+      cursorline = true;
+      title = true;
+      exrc = true;
+
+      # Misc
+      wrap = false;
+      splitright = true;
+      showmode = false;
     };
+    keymaps = [
+    {
+      options.desc = "Hide highlight for search predicate";
+      action = "<cmd>nohlsearch<CR>";
+      key = "<Leader>nh";
+      options.silent = true;
+    }
+    {
+      options.desc = "Go to next buffer";
+      action = "<cmd>bnext<CR>";
+      key = "<Leader>lb";
+      options.silent = true;
+    }
+    {
+      options.desc = "Go to prev buffer";
+      action = "<cmd>bprev<CR>";
+      key = "<Leader>hb";
+      options.silent = true;
+    }
+    ];
     globals = {
       mapleader = "<Space>";
     };
@@ -20,18 +65,21 @@ in {
         pplugin = name: { inherit name; };
       in [
         (pplugin "nvim-lsp")
-	(pplugin "async-path")
-	(pplugin "buffer")
-	(pplugin "calc")
-	(pplugin "conventionalcommits")
-	(pplugin "emoji")
-	(pplugin "spell")
+        (pplugin "async-path")
+        (pplugin "buffer")
+        (pplugin "calc")
+        (pplugin "conventionalcommits")
+        (pplugin "emoji")
+        (pplugin "spell")
       ];
     };
     plugins.compiler.enable = true;
     plugins.fzf-lua.enable = true;
     plugins.git-conflict.enable = true;
-    plugins.gitblame.enable = true;
+    plugins.gitblame = {
+      enable = true;
+      settings.enabled = false;
+    };
     plugins.gitsigns.enable = true;
     plugins.indent-blankline.enable = true;
     plugins.lualine.enable = true;
@@ -40,15 +88,15 @@ in {
       enable = true;
       grammarPackages = with pkgs.vimPlugins.nvim-treesitter.builtGrammars; [
         json
-        lua
-        markdown
-        nix
-        regex
-        toml
-        xml
-        yaml
-	rust
-	python
+          lua
+          markdown
+          nix
+          regex
+          toml
+          xml
+          yaml
+          rust
+          python
       ];
     };
     plugins.trouble.enable = true;
