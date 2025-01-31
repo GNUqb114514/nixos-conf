3f5a2298740f5b4913b68ecc29e20f50d3353401
qb114514 <GNUqb114514@outlook.com>
2025-01-31 19:37:29 +0800

    feat!: Remove cargo from system-wide confs

--- configuration.nix
+++ configuration.nix
@@ -93,8 +93,6 @@
     git
     neovim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     wget
-    cargo
-    rustc
     rclone
   ];
   environment.variables.EDITOR = "nvim";
