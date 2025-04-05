fe7ac5e687aea9ca19c823f2b286cc9455b78149
qb114514 <GNUqb114514@outlook.com>
2025-04-05 11:34:11 +0800

    chore(substituters): Disable configurations

--- configuration.nix
+++ configuration.nix
@@ -21,9 +21,9 @@
     "https://mirror.nju.edu.cn/nix-channels/store"
     # "https://mirrors.cqupt.edu.cn/nix-channels/store"
     "https://mirror.nyist.edu.cn/nix-channels/store"
-    "https://mirror.sjtu.edu.cn/nix-channels/store"
+    # "https://mirror.sjtu.edu.cn/nix-channels/store"
     "https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store"
-    "https://mirrors.cernet.edu.cn/nix-channels/store"
+    # "https://mirrors.cernet.edu.cn/nix-channels/store"
     # "https://nix-community.cachix.org"
   ];
 
