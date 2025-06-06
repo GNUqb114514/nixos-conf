44431f4f99bb276ef34b72b7be189a83abdcda2e
qb114514 <GNUqb114514@outlook.com>
2025-06-06 15:35:20 +0800

    Switch to default kitty text composition strategy

--- software-config/shell/terminal.nix
+++ software-config/shell/terminal.nix
@@ -2,10 +2,6 @@
   programs.alacritty.enable = true;
   programs.kitty.enable = true;
 
-  programs.kitty.settings = {
-    text_composition_strategy = "legacy";
-  };
-
   programs.alacritty.settings = {
     font = {
       size = 13;
