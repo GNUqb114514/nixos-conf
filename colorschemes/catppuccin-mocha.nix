92963b1494435cea8e97fffbef8e4cc74ceafa9b
qb114514 <GNUqb114514@outlook.com>
2025-06-21 09:47:51 +0800

    Make Comment highlight group brighter

--- colorschemes/catppuccin-mocha.nix
+++ colorschemes/catppuccin-mocha.nix
@@ -1,6 +1,16 @@
-{ pkgs, ... }: {
+{ pkgs, config, ... }: {
   stylix.enable = true;
   stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-mocha.yaml";
+
+  programs.nvf.settings.vim.highlight = {
+    "Comment" = {
+      fg = config.lib.stylix.colors.withHashtag.base04;
+    };
+    "TSComment" = {
+      fg = config.lib.stylix.colors.withHashtag.base04;
+      italic = true;
+    };
+  };
 
   imports = [ ./shared.nix ];
 }
