01e1c9114cdc24f49b14a4fd3c52bfe5c422601f
qb114514 <GNUqb114514@outlook.com>
2025-07-03 19:57:37 +0800

    Stop allowing unfree license

--- configuration.nix
+++ configuration.nix
@@ -31,8 +31,6 @@
     "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
   ];
 
-  nixpkgs.config.allowlistedLicenses = with lib.licenses; [ hl3 ];
-
   # Use the systemd-boot EFI boot loader.
   # boot.loader.systemd-boot.enable = true;
   # boot.loader.efi.canTouchEfiVariables = true;
