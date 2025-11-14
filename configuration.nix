eafd0da79c51afbc95c1ffff3b1ca71d6db27aaa
qb114514 <GNUqb114514@outlook.com>
2025-11-14 11:56:11 +0800

    chore(sources): Disable nix-community sources

--- configuration.nix
+++ configuration.nix
@@ -24,7 +24,7 @@
     "https://mirror.sjtu.edu.cn/nix-channels/store"
     "https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store"
     "https://mirrors.cernet.edu.cn/nix-channels/store"
-    "https://nix-community.cachix.org"
+    # "https://nix-community.cachix.org"
   ];
 
   nix.settings.trusted-public-keys = [
