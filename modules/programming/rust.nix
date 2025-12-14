e919ce9a605cf4b87b956158adc4d7066a2b867c
qb114514 <GNUqb114514@outlook.com>
2025-12-14 11:51:28 +0800

    feat(programming/rust): Isolate Rust environment to runCommand

--- modules/programming/rust.nix
+++ modules/programming/rust.nix
@@ -2,7 +2,6 @@
   pkgs,
   lib,
   config,
-  inputs,
   ...
 }: let
   cfg = config.user.programming.rust;
@@ -10,8 +9,26 @@ in {
   options.user.programming.rust = lib.mkEnableOption "Rust programming environment";
 
   config = lib.mkIf cfg {
-    home.packages = with pkgs; [
-      rust-bin.stable.latest.default
+    home.packages = [
+      (
+        let
+          packages = with pkgs; [
+            (rust-bin.stable.latest.default.override {
+              extensions = [ "rust-analyzer" "rust-std" ];
+            })
+          ];
+        in
+        pkgs.runCommand "env-rust"
+          {
+            buildinputs = packages;
+            nativeBuildInputs = [ pkgs.makeWrapper ];
+          }
+          ''
+            mkdir -p $out/bin/
+            ln -s ${pkgs.zsh}/bin/zsh $out/bin/env-rust
+            wrapProgram $out/bin/env-rust --prefix PATH : ${pkgs.lib.makeBinPath packages}
+          ''
+      )
     ];
 
     programs.nvf.settings.vim.languages.rust.enable = true;
