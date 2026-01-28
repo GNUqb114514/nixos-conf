c99ea2a87385ddb7d3181a70660bc9c76b862692
qb114514 <GNUqb114514@outlook.com>
2026-01-28 17:48:11 +0800

    feat(emacs): Add some custom variables

--- modules/emacs/default.nix
+++ modules/emacs/default.nix
@@ -28,6 +28,25 @@
         emacs-application-framework
       ] ++ (lib.concatMap (x: x epkgs) cfg.extraPackages));
       extraConfig = ''
+(setopt confirm-kill-emacs #'yes-or-no-p)
+(electric-pair-mode t)
+(add-hook 'prog-mode-hook #'show-paren-mode)
+(column-number-mode t)
+(global-auto-revert-mode t)
+(setopt make-backup-files nil)
+(setopt auto-save-default nil)
+(setopt byte-compile-auto-compile nil)
+(setopt load-prefer-newer t)
+(setopt create-lockfiles nil)
+(setopt recentf-save-file nil)
+(setopt load-prefer-newer t)
+(add-hook 'prog-mode-hook #'hs-minor-mode)
+(global-display-line-numbers-mode 1)
+(tool-bar-mode -1)
+(when (display-graphic-p) (toggle-scroll-bar -1))
+(setopt display-line-numbers-type 'relative)
+(setopt frame-resize-pixelwise t)
+
 (use-package good-scroll
   :if window-system
   :config (good-scroll-mode))
