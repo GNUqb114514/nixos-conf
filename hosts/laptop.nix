3bc9f010cb99529cceb5967bcb9aa8bedfb28ebe
qb114514 <GNUqb114514@outlook.com>
2026-02-16 17:23:00 +0800

    feat: Add disko

--- hosts/laptop.nix
+++ hosts/laptop.nix
@@ -29,12 +29,61 @@
         efiSupport = true;
       };
     };
+
+    disko.devices = {
+      disk = {
+        main = {
+          # No device config in favor of disko-install command-line
+          type = "disk";
+          content = {
+            type = "gpt";
+            partitions = {
+              ESP = {
+                size = "512M";
+                type = "EF00";
+                content = {
+                  type = "filesystem";
+                  format = "vfat";
+                  mountpoint = "/boot";
+                  mountOptions = ["umask=0077"];
+                };
+              };
+              swap = {
+                size = "8G";
+                content = {
+                  type = "swap";
+                  resumeDevice = true;
+                };
+              };
+              root = {
+                size = "100%";
+                content = {
+                  type = "btrfs";
+                  #format = "btrfs";
+                  subvolumes = {
+                    "@root" = {
+                      mountpoint = "/";
+                      mountOptions = ["compress=zstd:1" "discard=async"];
+                    };
+                    "@journal" = {
+                      mountpoint = "/var/log";
+                      mountOptions = ["compress=zstd:3" "discard=async"];
+                    };
+                  };
+                };
+              };
+            };
+          };
+        };
+      };
+    };
   };
   
   imports = [
     ../age.nix
 
     ./common.nix
-    ./hardware-configuration.nix
+
+    inputs.disko.nixosModules.disko
   ];
 }
