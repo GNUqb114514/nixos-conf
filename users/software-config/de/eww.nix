b312482b7509dcfcc1f2a4c51655e53ddf2ec3de
qb114514 <GNUqb114514@outlook.com>
2026-02-19 20:20:42 +0800

    chore: Remove orphan files
    A huge deletion...

--- users/software-config/de/eww.nix
+++
@@ -1,8 +0,0 @@
-{ config, ... }: {
-  programs.eww = {
-    enable = true;
-    configDir = config.lib.file.mkOutOfStoreSymlink "/home/qb114514/nixos-conf/software-config/de/eww/";
-  };
-
-  home.file.".local/share/fcitx5/lua/imeapi/extensions/emit-signal-to-bar.lua".source = ./emit-signal-to-bar.lua;
-}
