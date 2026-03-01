7a0dc2e96bfaf41c00c21b95251cb0cff05708c0
qb114514 <GNUqb114514@outlook.com>
2026-03-01 10:09:46 +0800

    fix(emacs): Only one component allowed in corfu

--- subflakes/hm/modules/emacs/basic.nix
+++ subflakes/hm/modules/emacs/basic.nix
@@ -19,6 +19,7 @@ in
         name = "orderless";
         custom = {
           completion-styles = "'(orderless)";
+          orderless-component-separator = ''"[ -]"'';
         };
       }
       {
